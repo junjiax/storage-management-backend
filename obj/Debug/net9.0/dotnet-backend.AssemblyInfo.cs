e0638a19de235e04f291394b2ee23d8f7844b0bb
junjiax <tuangiai52@gmail.com>
2025-11-13 23:14:18 +0700

    pulled main

--- obj/Debug/net9.0/dotnet-backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/dotnet-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b31f32e4bfd51798ecddee0d993a8247a0345e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164bd95d5db91fd2d3da26bc08bc4d1ee907fc03")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
