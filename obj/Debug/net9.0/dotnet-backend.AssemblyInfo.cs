3ec559806622d5ab3617388c1fe12a81b6cc9b34
junjiax <tuangiai52@gmail.com>
2025-12-08 14:05:08 +0700

    pulled commit update user

--- obj/Debug/net9.0/dotnet-backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/dotnet-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2847653a2b40f26cd643b8604d9999ea3f1159c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776271c20ac0a61fa168de1db0675c70e6659082")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
