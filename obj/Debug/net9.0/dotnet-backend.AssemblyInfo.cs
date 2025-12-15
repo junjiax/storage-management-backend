080f89bc170616f9f36330702f17130aacdc6518
phathociT <hatranduyphat2004@gmail.com>
2025-12-15 11:51:28 +0700

    [AdminDashboard]: Done!

--- obj/Debug/net9.0/dotnet-backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/dotnet-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4708af18eaca91dbab3d9a6958ceddaed377901b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b48e64254dc76ae1e1299c7791ab36b54f152ef3")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
